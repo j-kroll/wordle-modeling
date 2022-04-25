3194d37fdeef3827e1938daf56e1a78e9a865375
Julia Kroll <jkroll704@gmail.com>
2022-04-25 02:36:46 +0000

    Construct dictionary for transition matrix

--- model_wordle.py
+++ model_wordle.py
@@ -4,14 +4,14 @@ import re
 def get_embedding(wordle_emojis):
     embedding = []
     for line in wordle_emojis.split("\n"):
-        line_embedding = []
+        line_embedding = ""
         for char in line:
             if char in ["⬛","⬜"]:
-                line_embedding.append(0)
+                line_embedding += "0"
             elif char == "🟨":
-                line_embedding.append(1)
+                line_embedding += "1"
             elif char == "🟩":
-                line_embedding.append(2)
+                line_embedding += "2"
             else:
                 print("Warning: unknown character", char)
         embedding.append(line_embedding)
@@ -24,6 +24,7 @@ def main():
 
     # Read tweets
     num_tweets = 0
+    embeddings = []
     with open("wordle-tweets.csv") as f:
         csv_reader = csv.reader(f, delimiter=",")
         headers = next(csv_reader)
@@ -31,6 +32,7 @@ def main():
             wordle_id, tweet_id, tweet_date, tweet_username, tweet_text = row
             wordle_emojis = re.search("[⬛⬜🟨🟩\n]+", tweet_text).group(0).strip("\n")
             wordle_embedding = get_embedding(wordle_emojis)
+            embeddings.append(wordle_embedding)
             num_tweets += 1
     print("Processed {} tweets".format(num_tweets))
 
@@ -41,6 +43,21 @@ def main():
             date, wordle_id, word = line.strip("\n").split("\t")
             solutions[wordle_id] = word
     print("Processed {} solutions".format(len(solutions)))
+
+    # Construct dictionary for transition matrix
+    chain = {}
+    num_transitions = 0
+    for emb in embeddings:
+        for i in range(len(emb) + 1):
+            start_state = "<START>" if i == 0 else emb[i - 1]
+            end_state = "<END>" if i == len(emb) else emb[i]
+            if not start_state in chain:
+                chain[start_state] = {}
+            if not end_state in chain[start_state]:
+                chain[start_state][end_state] = 0
+                num_transitions += 1
+            chain[start_state][end_state] += 1
+    print("Found {} unique state transitions".format(num_transitions))
 
 if __name__ == '__main__':
     main()
